6aa7ebf47b138de26e809acca4b61d03c67949ec
Sam Tyler <samtyler905@gmail.com>
2026-06-03 13:22:03 +0100

    Initial Commit

---
+++ JustVisiting/JustVisitingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustVisitingApp.swift
+//  JustVisiting
+//
+//  Created by Sam Tyler on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct JustVisitingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JustVisitingTests/JustVisitingTests.swift
@@ -0,0 +1,19 @@
+//
+//  JustVisitingTests.swift
+//  JustVisitingTests
+//
+//  Created by Sam Tyler on 03/06/2026.
+//
+
+import Testing
+@testable import JustVisiting
+
+struct JustVisitingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
